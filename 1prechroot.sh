5ff25ceb819823b94cd7f002bbe74e1a52578f1a
m00E10 <93796498+m00E10@users.noreply.github.com>
2022-04-02 22:39:13 -0400

    Update 1prechroot.sh

--- 1prechroot.sh
+++ 1prechroot.sh
@@ -1,185 +1,56 @@
-#!/bin/bash
-# Dual boot, btrfs, linux hardened, sway with i3status-rust
-
-function repo_setup {
-  pacman -Sy
-  pacman -S parted artix-archlinux-support
-  pacman-key --populate archlinux
-  echo "[omniverse]" >> /etc/pacman.conf
-  echo "Server = http://omniverse.artixlinux.org/\$arch" >> /etc/pacman.conf
-  echo "[extra]" >> /etc/pacman.conf
-  echo "Include = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf
-  echo "[community]" >> /etc/pacman.conf
-  echo "Include = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf
-  echo "[multilib]" >> /etc/pacman.conf
-  echo "Include = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf
-  pacman -Sy
-
-  disk_setup
-}
+DRIVE=sda
+HOSTE=artix
+ADMIN=admin
+USER1=user
 
 function disk_setup {
-  fdisk -l | grep /dev
-  echo -e "\033[0;33mInput the drive you wish to install Artix to.\033[0m"
-  echo -e "\033[0;33mExample: sda\033[0m"
-  read DRIVE
-  echo "DRIVE=$DRIVE" >> vars
+(
+echo o     # Create new DOS partition layout
+echo n     # Create 100MB BOOT partition
+echo p
+echo 1
+echo  
+echo +100M
+echo n     # Create 16GB SWAP partition
+echo p
+echo 2
+echo  
+echo +16G
+echo n     # Create ROOT partition out of remaining space
+echo p
+echo 3
+echo  
+echo  
+echo a     # Set first partition as bootable
+echo 1
+echo t     # Set second partition as type 82 (swap)
+echo 2
+echo 82
+echo p
+echo w
+) | fdisk /dev/$DRIVE
 
-  echo -e "\033[0;33mDo you want to setup LUKS encryption?\033[0m"
-  echo -e "\033[0;33m1. Yes\033[0m"
-  echo -e "\033[0;33m2. No\033[0m"
-  
-  while [ "$luks_answer" != "1" ]; do
-    read luks_answer
-  	if [ "$luks_answer" == "1" ]; then
-		echo "ENCRYPTED=1" >> vars
-  		encryption_setup
-  	elif [ "$luks_answer" == "2" ]; then
-		echo "ENCRYPTED=0" >> vars
-		regular_setup
-    fi
-  done
+	mkfs.ext4 -L BOOT /dev/$DRIVE\1
+	mkswap    -L SWAP /dev/$DRIVE\2
+	mkfs.ext4 -L ROOT /dev/$DRIVE\3
+
+	mkdir  /mnt/boot
+	mount  /dev/$DRIVE\1  /mnt/boot
+	swapon /dev/$DRIVE\2
+	mount  /dev/$DRIVE\3  /mnt
 }
 
-function encryption_setup {
-  MPATH=/dev/mapper/artix
-  # Set up LUKS encryption
-  parted -s /dev/$DRIVE mklabel msdos
-  echo -e "\033[0;31mBe aware: Any currently installed Operating Systems will DIE after this step\033[0m"
-  echo -e "\033[0;33mWill you be dual booting?\033[0m"
-  echo -e "\033[0;33m1. Yes (Uses first 50% of drive)\033[0m"
-  echo -e "\033[0;33m2. No  (Uses 100% of drive)\033[0m"
-
-  while [[ "$userans" != "1" && "$userans" != "2" ]]; do
-    read userans
-    if [ "$userans" == "1" ]; then
-      parted -s /dev/$DRIVE mkpart primary 8192s 50%
-      cryptsetup --verbose --type luks1 --cipher serpent-xts-plain64 --key-size 512 --hash whirlpool --iter-time 10000 --use-random --verify-passphrase luksFormat /dev/$DRIVE\1
-      cryptsetup open /dev/$DRIVE\1 artix
-      # Does grub support luks2 yet?
-    elif [ "$userans" == "2" ]; then
-      parted -s /dev/$DRIVE mkpart primary 8192s 100%
-      cryptsetup --verbose --type luks1 --cipher serpent-xts-plain64 --key-size 512 --hash whirlpool --iter-time 10000 --use-random --verify-passphrase luksFormat /dev/$DRIVE\1
-      cryptsetup open /dev/$DRIVE\1 artix
-    fi
-  done
-
-  btrfs_setup
-}
-
-function regular_setup {
-  MPATH=/dev/$DRIVE
-  parted -s $MPATH mklabel msdos
-  echo -e "\033[0;31mBe aware: Any currently installed Operating Systems will DIE after this step\033[0m"
-  echo -e "\033[0;33mWill you be dual booting?\033[0m"
-  echo -e "\033[0;33m1. Yes (Uses first 50% of drive)\033[0m"
-  echo -e "\033[0;33m2. No  (Uses 100% of drive)\033[0m"
-
-  while [[ "$userans" != "1" && "$userans" != "2" ]]; do
-    read userans
-    if [ "$userans" == "1" ]; then
-      parted -s $MPATH mkpart primary 8192s 50%
-    elif [ "$userans" == "2" ]; then
-      parted -s $MPATH mkpart primary 8192s 100%
-    fi
-  done
-
-  btrfs_setup
-}
-
-function btrfs_setup {
-  # Create filesystems and subvolumes
-  mkfs -t btrfs --force -L artix $MPATH
-  mount -t btrfs -o compress=lzo $MPATH /mnt
-  btrfs subvolume create /mnt/@
-  btrfs subvolume create /mnt/@home
-  btrfs subvolume create /mnt/@snapshots
-  btrfs subvolume create /mnt/@swap
-  
-  umount /mnt
-  mkdir /swap
-  # Set mount options
-  o=defaults,x-mount.mkdir,compress=lzo,ssd,noatime
-
-  # Remount partitions
-  mount -o compress=lzo,subvol=@,$o $MPATH /mnt
-  mount -o compress=lzo,subvol=@home,$o $MPATH /mnt/home
-  mount -o compress=lzo,subvol=@snapshots,$o $MPATH /mnt/.snapshots
-  mount -o subvol=@swap $MPATH /swap
-  # mount -o subvol=@swap /dev/mapper/artix /swap
-  
-  # Make swap file
-  touch /swap/swapfile
-  chmod 600 /swap/swapfile
-  chattr +C /swap/swapfile
-  echo "Making 8GB swap file"
-  dd if=/dev/zero of=/swap/swapfile bs=1M count=8192 status=progress
-  mkswap /swap/swapfile
-  swapon /swap/swapfile
-  # UUID=$(blkid $MPATH | grep -o '\<UUID=[^[:blank:]]*' | cut -c 7- | rev | cut -c 2- | rev)
-  # echo "UUID=$UUID /swap btrfs subvol=@swap 0 0" >> /etc/fstab
-  # echo "/swap/swapfile none swap sw 0 0" >> /etc/fstab
-
-  basestrap_setup
-}
-
-function basestrap_setup {
-
-	while [[ "$CPU" != "1" && "$CPU" != "2" ]]; do 
-	  echo -e "\033[0;33mWhat brand is your CPU?\033[0m"
-	  echo -e "\033[0;31m1. AMD\033[0m"
-	  echo -e "\033[0;34m2. Intel\033[0m"
-		read CPU
-	  if [ "$CPU" == "1" ]; then
-	    cpu_package=amd-ucode
-		elif [ " $CPU" == "2" ]; then
-			cpu_package=intel-ucode
-		fi
-	done
-	echo "CPU=$CPU" >> vars
-
-	while [[ "$INIT" != "1" && "$INIT" != "2" && "$INIT" != "3" ]]; do
-	  echo -e "\033[0;33mWhat init system do you want to use?\033[0m"
-	  echo -e "\033[0;34m1. Runit\033[0m"
-	  echo -e "\033[0;37m2. S6\033[0m"
-	  echo -e "\033[0;31m3. OpenRC\033[0m"
-	  read INIT
-	  if [ "$INIT" == "1" ]; then
-			init_system=runit
-	  elif [ "$INIT" == "2" ]; then
-			init_system=s6
-	  elif [ "$INIT" == "3" ]; then
-			init_system=openrc
-	  fi 
-	done
-	echo "INIT=$INIT" >> vars
+function base_strap {
+	basestrap   /mnt base base-devel openrc elogind-openrc linux-hardened \
+	                 linux-hardened-headers linux-firmware wget
+	fstabgen -L /mnt                                             >> /mnt/etc/fstab
+	echo "LABEL=BOOT    /boot    ext4    default,noatime    0 2" >> /mnt/etc/fstab
 	
-        base_packages="base base-devel linux-hardened linux-hardened-headers usbctl linux-firmware wget vim btrfs-progs grub mlocate dosfstools cryptsetup doas networkmanager network-manager-applet networkmanager-$init_system elogind-$init_system"
-	basestrap /mnt $base_packages $cpu_package
-
-	fstabgen -U /mnt >> /mnt/etc/fstab
-
-	setup_next
+	wget https://raw.githubusercontent.com/m00E10/install-artix/main/2chrooted.sh
+	mv 2chrooted.sh /mnt/2chrooted.sh
+	echo "Now run bash 2chrooted.sh"
+	artix-chroot /mnt
 }
 
-function setup_next {
-  # Merge previous variables and place 2nd script in chroot environment
-  cp vars tmpvars
-  cat 2chrooted.sh >> tmpvars
-  mv tmpvars 2chrooted.sh
-  
-  mv dotfiles /mnt
-  mv sysfiles/* /mnt
-  mv *.sh /mnt
-  rm -rf sysfiles
-  rm *.md
-  rm -rf .git
-  mv .* /mnt
-  mv vars /mnt
-  echo -e "\033[0;33mNow run the following commands manually\033[0m"
-  echo -e "\033[1;36martix-chroot /mnt\033[0m"
-  echo -e "\033[1;36mbash 2chrooted.sh\033[0m"
-  exit
-}
-
-repo_setup
+disk_setup
+base_strap	
